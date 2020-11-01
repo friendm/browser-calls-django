bd0679f56b2600c1bf2d58bb060e0b940e673832
Mike Friend <sodium56@gmail.com>
2020-11-01 12:59:13 -0500

    Update common.py

--- twilio_sample_project/settings/common.py
+++ twilio_sample_project/settings/common.py
@@ -104,7 +104,12 @@ WSGI_APPLICATION = 'twilio_sample_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 import dj_database_url
 db_from_env=dj_database_url.config(conn_max_age=600)
