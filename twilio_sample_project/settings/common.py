2eceda67496b4c285f4fc3e748285bf4a4a45b3e
Mike Friend <sodium56@gmail.com>
2020-11-01 12:54:36 -0500

    Update common.py

--- twilio_sample_project/settings/common.py
+++ twilio_sample_project/settings/common.py
@@ -108,7 +108,7 @@ WSGI_APPLICATION = 'twilio_sample_project.wsgi.application'
 
 import dj_database_url
 db_from_env=dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASE['default'].update(db_from_env)
              
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
