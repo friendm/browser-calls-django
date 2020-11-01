70cc0b90e2d7f7b5607bf6fa095502b3592e49c4
Mike Friend <sodium56@gmail.com>
2020-11-01 15:21:43 -0500

    TRYING TO GET MIGRATION TO WORK
    D

--- twilio_sample_project/settings/production.py
+++ twilio_sample_project/settings/production.py
@@ -6,7 +6,7 @@ Production settings
 
 from .common import *
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Allow all hosts, so we can run on PaaS's like Heroku
 #ALLOWED_HOSTS = ['https://insurancehold.herokuapp.com/']
