ae2f670c340ac331f588c86c6afceb87a97a5406
Mike Friend <sodium56@gmail.com>
2020-11-10 19:37:24 -0500

    Update views.py

--- browser_calls/views.py
+++ browser_calls/views.py
@@ -55,14 +55,14 @@ def call(request):
     response = VoiceResponse()
 
     dial = response.dial(caller_id=settings.TWILIO_NUMBER)
-    gather = Gather(num_digits=1)
-    gather.say('For sales, press 1. For support, press 2.')
-    response.append(gather)
+
     
         # If the browser sent a phoneNumber param, we know this request
     # is a support agent trying to call a customer's phone
     if 'phoneNumber' in request.POST:
         dial.number(request.POST['phoneNumber'])
+    gather = Gather(num_digits=1)
+    gather.say('For sales, press 1. For support, press 2.')
     
 
 
