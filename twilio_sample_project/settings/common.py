a5fd18ff5c9aaa862dd4046955f9c474de077867
Mike Friend <sodium56@gmail.com>
2020-11-10 19:09:03 -0500

    Update common.py

--- twilio_sample_project/settings/common.py
+++ twilio_sample_project/settings/common.py
@@ -20,7 +20,7 @@ from dotenv import load_dotenv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = []
