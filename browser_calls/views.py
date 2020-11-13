54b89e9d4586e08aaf3b8d7c279de2e1e14f4e56
Mike Friend <sodium56@gmail.com>
2020-11-12 20:20:41 -0500

    Update views.py

--- browser_calls/views.py
+++ browser_calls/views.py
@@ -56,6 +56,9 @@ def call(request):
     
     dial = response.dial(caller_id=settings.TWILIO_NUMBER)
     #response.say('hello this is mike friend', voice='woman', language='fr-FR')
+    g=Gather(num_digits=1)
+    g.say('For sales, press 1. For support, press 2.')
+    dial.append(g)
 
 
     
@@ -63,9 +66,7 @@ def call(request):
     # is a support agent trying to call a customer's phone
     if 'phoneNumber' in request.POST:
         dial.number(request.POST['phoneNumber'])
-    g=Gather(num_digits=1)
-    g.say('For sales, press 1. For support, press 2.')
-    dial.append(g)
+
 
     
 
